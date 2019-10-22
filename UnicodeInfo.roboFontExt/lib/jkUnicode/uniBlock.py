6c000187e0e6b60ae85fc14c6b9312e79c0cd92d
Jens Kutilek <webmail@kutilek.de>
2019-10-22 18:00:16 +0200

    Update jkUnicode to v1.5.0

--- UnicodeInfo.roboFontExt/lib/jkUnicode/uniBlock.py
+++ UnicodeInfo.roboFontExt/lib/jkUnicode/uniBlock.py
@@ -8,36 +8,36 @@ uniBlockToName = RangeDict(uniBlocks)
 # The reverse mapping of names to blocks
 uniNameToBlock = {}
 for k, v in uniBlockToName.items():
-	if not v in uniNameToBlock:
-		uniNameToBlock[v] = k
-	else:
-		print("ERROR: Duplicate block name: %s" % v)
+    if not v in uniNameToBlock:
+        uniNameToBlock[v] = k
+    else:
+        print("ERROR: Duplicate block name: %s" % v)
 
 
 def get_block(codepoint):
-	try:
-		return uniBlockToName[codepoint]
-	except KeyError:
-		return None
+    try:
+        return uniBlockToName[codepoint]
+    except KeyError:
+        return None
 
 
 def get_codepoints_min_max(block_name):
-	try:
-		return uniNameToBlock[block_name]
-	except KeyError:
-		return None
+    try:
+        return uniNameToBlock[block_name]
+    except KeyError:
+        return None
 
 
 def get_codepoints(block_name):
-	try:
-		low, high = uniNameToBlock[block_name]
-		return range(low, high + 1)
-	except KeyError:
-		return []
+    try:
+        low, high = uniNameToBlock[block_name]
+        return range(low, high + 1)
+    except KeyError:
+        return []
 
 
 if __name__ == "__main__":
-	print(get_block(0x4ff))
-	print(get_block(0x500))
-	print(get_codepoints_min_max("Cyrillic Supplement"))
-	print(get_codepoints("Cyrillic Supplement"))
+    print(get_block(0x4ff))
+    print(get_block(0x500))
+    print(get_codepoints_min_max("Cyrillic Supplement"))
+    print(get_codepoints("Cyrillic Supplement"))
